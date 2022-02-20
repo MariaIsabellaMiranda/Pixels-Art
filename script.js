0bd3d483a324bc7a00d967400b20b3e08d6ab451
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-20 19:43:20 -0300

    Finalizados requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
-let quadro = document.querySelector('#pixel-board');
+const quadro = document.querySelector('#pixel-board');
+const paleta = document.querySelector('#color-palette');
+let cor = 'rgb(0, 0, 0)';
+const botao = document.querySelector('#clear-board');
 
-for (let index = 0; index < 5; index += 1) {
-    let linha = document.createElement('section')
-    quadro.appendChild(linha);
-    for (let index2 = 0; index2 < 5; index2 += 1) {
+window.onload = function() {
+  criaQuadro()
+}
+  function criaQuadro(){
+    for (let index = 0; index < 5; index += 1) {
+      let linha = document.createElement('section')
+      quadro.appendChild(linha);
+      for (let index2 = 0; index2 < 5; index2 += 1) {
         let novoPixel = document.createElement('div');
         novoPixel.className = ('pixel');
         linha.appendChild(novoPixel);
+      }
     }
-}
+  }
+ 
+
+  paleta.addEventListener('click', function(event){
+   const el = event.target
+   if (el === paleta) {
+     return
+   }
+   const previousSelected = document.querySelector('.selected')
+   previousSelected.classList.remove('selected')
+   el.classList.add('selected');
+   cor = getComputedStyle(el).backgroundColor
+   console.log(cor);
+  })
+  
+  quadro.addEventListener('click', function(event){
+    const el = event.target
+    if (el.classList.contains('pixel')) {
+      el.style.backgroundColor = cor;
+    }
+    console.log(el.classList.contains('pixel'));
+  })
+
+  botao.addEventListener('click', function(event){
+    quadro.innerHTML = null
+    criaQuadro()
+  })
