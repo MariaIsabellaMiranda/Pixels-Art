bab4b8a29702d24570f2e12cb72e61a4ea05e481
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-21 08:13:11 -0300

    Retirado console.log esquecidos

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let cor = 'rgb(0, 0, 0)';
 const botao = document.querySelector('#clear-board');
 
 window.onload = function() {
-  criaQuadro()
+  criaQuadro();
 }
   function criaQuadro(){
     for (let index = 0; index < 5; index += 1) {
-      let linha = document.createElement('section')
+      let linha = document.createElement('section');
       quadro.appendChild(linha);
       for (let index2 = 0; index2 < 5; index2 += 1) {
         let novoPixel = document.createElement('div');
@@ -17,29 +17,26 @@ window.onload = function() {
       }
     }
   }
- 
 
   paleta.addEventListener('click', function(event){
-   const el = event.target
+   const el = event.target;
    if (el === paleta) {
-     return
+     return;
    }
-   const previousSelected = document.querySelector('.selected')
-   previousSelected.classList.remove('selected')
+   const previousSelected = document.querySelector('.selected');
+   previousSelected.classList.remove('selected');
    el.classList.add('selected');
-   cor = getComputedStyle(el).backgroundColor
-   console.log(cor);
+   cor = getComputedStyle(el).backgroundColor;
   })
   
   quadro.addEventListener('click', function(event){
-    const el = event.target
+    const el = event.target;
     if (el.classList.contains('pixel')) {
       el.style.backgroundColor = cor;
-    }
-    console.log(el.classList.contains('pixel'));
-  })
+    };
+  });
 
-  botao.addEventListener('click', function(event){
-    quadro.innerHTML = null
-    criaQuadro()
-  })
+  botao.addEventListener('click', function(){
+    quadro.innerHTML = null;
+    criaQuadro();
+  });
